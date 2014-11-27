8ef984e35af5927df8457b93b5b755ea8d3becf2
aritz.justice@gmail.com <aritz.justice@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-11-27 07:57:17 +0000

    Initial import.

---
+++ src/com/ipartek/formacion/helloweb/controller/LogoutServlet.java
@@ -0,0 +1,55 @@
+package com.ipartek.formacion.helloweb.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogoutServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		request.getRequestDispatcher("link.html").include(request, response);
+
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		out.print("You are successfully logged out!");
+
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
