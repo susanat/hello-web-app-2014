b2b4d7ddec1c50e6c1ea9fb76b60a91437b2ede2
mario.alsol@gmail.com <mario.alsol@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-11-27 07:46:27 +0000

    Primera importación del proyecto

---
+++ src/com/ipartek/formacion/helloweb/controller/LogoutServlet.java
@@ -0,0 +1,54 @@
+package com.ipartek.formacion.helloweb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.formacion.helloweb.Constantes;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    RequestDispatcher dispatch = null;
+    HttpSession session = null;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+	super();
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	// recuperar sesion
+	session = request.getSession();
+	session.removeAttribute(Constantes.USER_SESSION);
+	dispatch = request.getRequestDispatcher(Constantes.JSP_LOGIN);
+	dispatch.forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	doGet(request, response);
+    }
+
+}
