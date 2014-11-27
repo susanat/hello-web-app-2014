af87a6dd22cf2c464be16a59f435f09aa0d1e109
josebacarr@gmail.com <josebacarr@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-11-27 07:46:27 +0000

    Commit inicial

---
+++ branches/joseba/src/com/ipartek/formacion/helloweb/Constantes.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.helloweb;
+
+public class Constantes {
+    public static final String USER = "admin";
+    public static final String PASS = "admin";
+    public static final String USER_SESSION = "user_session";
+
+    public static final String PATH_LOGIN = "login";
+
+    public static final String JSP_SALUDO = "saludo.jsp";
+    public static final String JSP_LOGIN = "login.jsp";
+
+    public static final String PARAMETRO_USER = "user";
+    public static final String PARAMETRO_PASS = "pass";
+
+    public static final String MSG_KEY = "msg";
+    public static final String MSG_LOGIN_INCORRECT = "Usuario o contraseña incorrecta";
+}
