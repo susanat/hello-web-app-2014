46af6b8ed26da37415b9bf451edda71959569a30
josebacarr@gmail.com <josebacarr@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-11-27 12:13:27 +0000

    Commit del 27/11/2014
    Hemos visto login con roles y detalles de la request

--- branches/joseba/src/com/ipartek/formacion/helloweb/Constantes.java
+++ branches/joseba/src/com/ipartek/formacion/helloweb/Constantes.java
@@ -1,18 +1,25 @@
 package com.ipartek.formacion.helloweb;
 
 public class Constantes {
-    public static final String USER = "admin";
-    public static final String PASS = "admin";
+    public static final String USER_ADMIN_NAME = "admin";
+    public static final String USER_ADMIN_PASS = "admin";
     public static final String USER_SESSION = "user_session";
+
+    public static final String USUARIO = "usuario";
+    public static final String USUARIO_PASS = "pass";
 
     public static final String PATH_LOGIN = "login";
 
     public static final String JSP_SALUDO = "saludo.jsp";
     public static final String JSP_LOGIN = "login.jsp";
+    public static final String JSP_BACKOFFICE = "backoffice/index.jsp";
 
     public static final String PARAMETRO_USER = "user";
     public static final String PARAMETRO_PASS = "pass";
 
     public static final String MSG_KEY = "msg";
     public static final String MSG_LOGIN_INCORRECT = "Usuario o contraseña incorrecta";
+    public static final String MSG_LOGOUT = "Hasta la vista";
+    public static final String MSG_UNAUTHORIZED = "No le esta permitido el acceso";
+
 }
