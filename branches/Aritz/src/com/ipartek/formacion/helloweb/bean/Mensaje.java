f056b4e3cb4f44c07b2bd570f9af8457ab773e5b
aritz.justice@gmail.com <aritz.justice@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-12-05 11:16:54 +0000

    nuevo bean de mensajes

---
+++ branches/Aritz/src/com/ipartek/formacion/helloweb/bean/Mensaje.java
@@ -0,0 +1,58 @@
+package com.ipartek.formacion.helloweb.bean;
+
+public class Mensaje {
+
+	public static final String MSG_TYPE_SUCCESS = "success";
+	public static final String MSG_TYPE_INFO = "info";
+	public static final String MSG_TYPE_WARNING = "warning";
+	public static final String MSG_TYPE_DANGER = "danger";
+
+	// TODO constantes para los codigos http
+
+	String msg; // literal del mensaje
+	String type; // tipo de mensaje success,info,warning,danger
+	int code; // codigo http
+
+	public Mensaje(String msg) {
+		super();
+		this.msg = msg;
+	}
+
+	public Mensaje(String msg, String type) {
+		super();
+		this.msg = msg;
+		this.type = type;
+	}
+
+	public Mensaje(String msg, String type, int code) {
+		super();
+		this.msg = msg;
+		this.type = type;
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+}
