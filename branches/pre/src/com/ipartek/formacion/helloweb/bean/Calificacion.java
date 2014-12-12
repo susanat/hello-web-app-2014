ce5722b587ade07a96b521c1e95ed76bc8ecbb41
fransanchezfsg@gmail.com <fransanchezfsg@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-12-12 08:31:42 +0000

    Modelo Calificaciones y clase representa una calificación

---
+++ branches/pre/src/com/ipartek/formacion/helloweb/bean/Calificacion.java
@@ -0,0 +1,58 @@
+package com.ipartek.formacion.helloweb.bean;
+
+/**
+ * Clase para representar una calificacion.
+ * 
+ * @author Fran
+ *
+ */
+public class Calificacion {
+	private int id;
+	private int clave;
+	private String descripcion;
+
+	/**
+	 * Valor por defecto en el id
+	 */
+	public static final int ID_NULL = -1;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param clave
+	 *            {@code Int} valor de la calificación.
+	 * @param descripcion
+	 *            {@code String} descripcion de la calificación
+	 */
+	public Calificacion(int clave, String descripcion) {
+		super();
+		this.id = ID_NULL;
+		this.clave = clave;
+		this.descripcion = descripcion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getClave() {
+		return clave;
+	}
+
+	public void setClave(int clave) {
+		this.clave = clave;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
