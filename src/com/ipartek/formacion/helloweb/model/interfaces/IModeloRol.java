f7caf3ec512672558e3f6d0c03cdee1ef481d1ba
srnsecreta <srnsecreta@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-12-12 09:43:40 +0000

    Role: Añadido default y borrado RoleServlet: Adaptacion al modelo Y otros cambios...

--- src/com/ipartek/formacion/helloweb/model/interfaces/IModeloRol.java
+++ src/com/ipartek/formacion/helloweb/model/interfaces/IModeloRol.java
@@ -1,19 +1,44 @@
 package com.ipartek.formacion.helloweb.model.interfaces;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.ipartek.formacion.helloweb.bean.Role;
 
 public interface IModeloRol {
 
-    ArrayList<Role> getAll();
+	/**
+	 * Obtiene una lista de Role
+	 * @return List<Role> lista de role
+	 */
+    public List<Role> getAll();
 
-    Role getByID(int id);
+    /**
+     * Obtiene un Role dado el identificador
+     * @param id Identificador del rol
+     * @return Role con el rol o null si no ha encontrado
+     */
+    public Role getByID(int id) throws Exception;
 
-    int insert(Role r);
+    /**
+     * Inserta un objeto de tipo Role
+     * @param r Objeto a insertar
+     * @return Integer , mayor de 0 se ha insertado el objeto 
+     */
+    public Role insert(Role r) throws Exception;
 
-    int update(Role r);
+    /**
+     * Actualiza un objeto de tipo Role
+     * @param r Objeto a actualizar
+     * @return
+     */
+    public Role update(Role r) throws Exception;
 
-    boolean delete(int id);
+    /**
+     * Elimina un objeto por su identificador
+     * @param id
+     * @return
+     */
+    public boolean delete(int id) throws Exception;
 
 }
