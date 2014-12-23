3d5c3e187c6efaa565ea164cee36f05a6ea15115
aritz.justice@gmail.com <aritz.justice@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2014-12-23 11:55:31 +0000

    selectoption tag

--- src/com/ipartek/formacion/helloweb/tag/TagHello2.java
+++ src/com/ipartek/formacion/helloweb/tag/TagHello2.java
@@ -1,29 +1,31 @@
 package com.ipartek.formacion.helloweb.tag;
 
-import java.io.IOException;
-
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class TagHello2 extends TagSupport {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -1852184443836896585L;
 	private String nombre;
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 	@Override
 	public int doEndTag() throws JspException {
 		try {
-			pageContext.getOut().print("Saluda desconocido");
-			if (nombre != null) {
-				pageContext.getOut().print("saluda " + nombre + "!");
-			}
-		} catch (IOException e) {
-			throw new JspException("Error: IOException" + e.getMessage());
+			JspWriter out = pageContext.getOut();
+			out.print("saluda " + nombre);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return SKIP_BODY;
-	}
+		return EVAL_PAGE;
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
 	}
 
 }
