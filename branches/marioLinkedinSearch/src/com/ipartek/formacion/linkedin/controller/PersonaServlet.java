e4d0573fdb46b9f62578c8be2525caceb35bce2a
mario.alsol@gmail.com <mario.alsol@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-08 12:19:52 +0000

    Actualiza y borra registros de la BD. Añadidos dos metodos para abrir y cerrar la conexión en PersonaServlet

--- branches/marioLinkedinSearch/src/com/ipartek/formacion/linkedin/controller/PersonaServlet.java
+++ branches/marioLinkedinSearch/src/com/ipartek/formacion/linkedin/controller/PersonaServlet.java
@@ -27,94 +27,13 @@ public class PersonaServlet extends HttpServlet {
 	    HttpServletResponse response) throws ServletException, IOException {
 	request.setCharacterEncoding("UTF-8");
 	// conectar BBDD
-	String resultado = conectar();
+	String resultado = consultarPersonas();
 
 	// pasar attributo resultado
 	request.setAttribute("personas", resultado);
 	// forward a jsp de busqueda
 	request.getRequestDispatcher("listadoPersonas.jsp").forward(request,
 		response);
-    }
-
-    private String conectar() {
-	String personas = "";
-	Connection conexion = null;
-	Statement st = null;
-	ResultSet rs = null;
-	try {
-	    /*
-	     * Class.forName("com.mysql.jdbc.Driver"); conexion =
-	     * DriverManager.getConnection( "jdbc:mysql://localhost/test",
-	     * "root", "");
-	     */
-	    InitialContext ctx = new InitialContext();
-	    DataSource ds = (DataSource) ctx
-		    .lookup("java:comp/env/jdbc/TestDB");
-	    conexion = ds.getConnection();
-
-	    st = conexion.createStatement();
-	    rs = st.executeQuery("SELECT * FROM persona");
-
-	    while (rs.next()) {
-		personas += "<form action='persona' method='post'>";
-
-		personas += "<input type='hidden' name='id' value='"
-			+ rs.getInt("id") + "'>";
-		personas += "<input type='text' name='nombre' value='"
-			+ rs.getString("nombre") + "'>";
-		personas += "<input type='text' name='apellidos' value='"
-			+ rs.getString("apellidos") + "'>";
-		personas += "<input type='text' name='edad' value='"
-			+ rs.getInt("edad") + "'>";
-		personas += "<input type='hidden' name='operacion' value='2'>";
-		personas += "<br>";
-		personas += "<input type='submit' value='Actualizar'>";
-
-		personas += "</form>";
-
-		personas += "<form action='persona' method='post'>";
-		personas += "<input type='hidden' name='id' value='"
-			+ rs.getInt("id") + "'>";
-		personas += "<input type='hidden' name='operacion' value='3'>";
-		personas += "<input type='submit' value='Eliminar'>";
-		personas += "</form>";
-
-		personas += "<br>";
-	    }
-
-	} catch (Exception e) {
-
-	    e.printStackTrace();
-
-	} finally { // cerrar todos los objetos creados para el acceso de BBDD
-	    // cerrar ResultSet
-	    if (rs != null) {
-		try {
-		    rs.close();
-		} catch (Exception e2) {
-		    e2.printStackTrace();
-		}
-	    }
-	    // cerrar statements
-	    if (st != null) {
-		try {
-		    st.close();
-		} catch (Exception e2) {
-		    e2.printStackTrace();
-		}
-	    }
-	    // cerrar conexion
-	    if (conexion != null) {
-		try {
-		    conexion.close();
-		} catch (Exception e2) {
-		    e2.printStackTrace();
-		}
-	    }
-	}
-
-	return personas;
-
     }
 
     /**
@@ -128,12 +47,14 @@ public class PersonaServlet extends HttpServlet {
 	String nombre = "";
 	String apellidos = "";
 	String id = "";
+	String foto = "";
 	String op = request.getParameter("operacion");
 
 	if (op.equals("1")) {
 	    nombre = request.getParameter("nombre");
 	    apellidos = request.getParameter("apellidos");
-	    if (insertar(nombre, apellidos)) {
+	    foto = request.getParameter("foto");
+	    if (insertar(nombre, apellidos, foto)) {
 		System.out.println("bien insertada");
 	    } else {
 		System.out.println("mal insertada");
@@ -167,19 +88,142 @@ public class PersonaServlet extends HttpServlet {
 
     }
 
-    private boolean borrar(int id) {
-	boolean correcto = false;
+    /**
+     * Crea la conexion con la BD
+     *
+     * @return conexion - La conexion
+     */
+    private Connection conectar() {
+
 	Connection conexion = null;
-	java.sql.PreparedStatement st = null;
-	ResultSet rs = null;
+
 	try {
+	    /*
+	     * Class.forName("com.mysql.jdbc.Driver"); conexion =
+	     * DriverManager.getConnection( "jdbc:mysql://localhost/test",
+	     * "root", "");
+	     */
 	    InitialContext ctx = new InitialContext();
 	    DataSource ds = (DataSource) ctx
 		    .lookup("java:comp/env/jdbc/TestDB");
 	    conexion = ds.getConnection();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	return conexion;
+    }
 
+    /**
+     * Cierra la conexion
+     *
+     * @param conexion
+     */
+    private void cerrarConexion(Connection conexion) {
+	// cerrar conexion
+	if (conexion != null) {
+	    try {
+		conexion.close();
+	    } catch (Exception e2) {
+		e2.printStackTrace();
+	    }
+	}
+
+    }
+
+    /**
+     * Consulta a la BD todas las personas que estén almacenadas
+     *
+     * @return personas - un string con todas las personas
+     */
+    private String consultarPersonas() {
+	String personas = "";
+	Statement st = null;
+	ResultSet rs = null;
+	Connection conexion = conectar();
+	try {
+
+	    st = conexion.createStatement();
+	    rs = st.executeQuery("SELECT * FROM persona");
+
+	    while (rs.next()) {
+		personas += "<div class='container'>";
+		personas += "<form action='persona' method='post'>";
+		personas += "<img alt='foto de perfil' src='"
+			+ rs.getString("url_foto")
+			+ "' class='pull-left margin-right' height='60' width='60'>";
+
+		personas += "<input type='hidden' name='id' value='"
+			+ rs.getInt("id") + "'>";
+
+		personas += "<input type='text' name='nombre' value='"
+			+ rs.getString("nombre") + "'>";
+
+		personas += "<input type='text' name='apellidos' value='"
+			+ rs.getString("apellidos") + "'>";
+
+		personas += "<input type='text' name='edad' value='"
+			+ rs.getInt("edad") + "'>";
+
+		personas += "<input type='hidden' name='operacion' value='2'>";
+		personas += "<br>";
+		personas += "<input type='submit' value='Actualizar' class='btn btn-primary btn-xs pull-left margin'>";
+
+		personas += "</form>";
+
+		personas += "<form action='persona' method='post' >";
+		personas += "<input type='hidden' name='id' value='"
+			+ rs.getInt("id") + "'>";
+		personas += "<input type='hidden' name='operacion' value='3'>";
+		personas += "<input type='submit' value='Eliminar' class='btn btn-danger btn-xs margin'>";
+		personas += "</form>";
+		personas += "</div>";
+		personas += "<br>";
+	    }
+
+	} catch (Exception e) {
+
+	    e.printStackTrace();
+
+	} finally { // cerrar todos los objetos creados para el acceso de BBDD
+	    // cerrar ResultSet
+	    if (rs != null) {
+		try {
+		    rs.close();
+		} catch (Exception e2) {
+		    e2.printStackTrace();
+		}
+	    }
+	    // cerrar statements
+	    if (st != null) {
+		try {
+		    st.close();
+		} catch (Exception e2) {
+		    e2.printStackTrace();
+		}
+	    }
+
+	    // cerrar conexion
+	    cerrarConexion(conexion);
+	}
+
+	return personas;
+
+    }
+
+    /**
+     * Borrar de la BD la tupla indicada por el id
+     *
+     * @param id
+     *            - identificador
+     * @return TRUE si ha borrado correctamente, false en caso contrario
+     */
+    private boolean borrar(int id) {
+	boolean correcto = false;
+	Connection conexion = conectar();
+	java.sql.PreparedStatement st = null;
+	ResultSet rs = null;
+	try {
 	    String sqlInsert = "DELETE FROM persona WHERE id=?";
-
 	    st = conexion.prepareStatement(sqlInsert);
 	    st.setInt(1, id);
 
@@ -208,28 +252,28 @@ public class PersonaServlet extends HttpServlet {
 		}
 	    }
 	    // cerrar conexion
-	    if (conexion != null) {
-		try {
-		    conexion.close();
-		} catch (Exception e2) {
-		    e2.printStackTrace();
-		}
-	    }
+	    cerrarConexion(conexion);
 	}
 
 	return correcto;
     }
 
+    /**
+     * Actualiza un registro en la BD
+     *
+     * @param id
+     *            - identificador
+     * @param nombre
+     * @param apellidos
+     * @param edad
+     * @return TRUE si ha borrado correctamente, false en caso contrario
+     */
     private boolean actualizar(int id, String nombre, String apellidos, int edad) {
 	boolean correcto = false;
-	Connection conexion = null;
+	Connection conexion = conectar();
 	java.sql.PreparedStatement st = null;
 	ResultSet rs = null;
 	try {
-	    InitialContext ctx = new InitialContext();
-	    DataSource ds = (DataSource) ctx
-		    .lookup("java:comp/env/jdbc/TestDB");
-	    conexion = ds.getConnection();
 
 	    String sqlInsert = "UPDATE persona SET nombre=?,apellidos=?,edad=? WHERE id=?";
 
@@ -264,36 +308,32 @@ public class PersonaServlet extends HttpServlet {
 		}
 	    }
 	    // cerrar conexion
-	    if (conexion != null) {
-		try {
-		    conexion.close();
-		} catch (Exception e2) {
-		    e2.printStackTrace();
-		}
-	    }
+	    cerrarConexion(conexion);
 	}
 
 	return correcto;
     }
 
-    private boolean insertar(String pNombre, String pApellidos) {
+    /**
+     * Inserta un registro en la BD
+     *
+     * @param pNombre
+     * @param pApellidos
+     * @return TRUE si ha borrado correctamente, false en caso contrario
+     */
+    private boolean insertar(String pNombre, String pApellidos, String pFoto) {
 	boolean correcto = false;
-	Connection conexion = null;
+	Connection conexion = conectar();
 	java.sql.PreparedStatement st = null;
 	ResultSet rs = null;
 	try {
-	    InitialContext ctx = new InitialContext();
-	    DataSource ds = (DataSource) ctx
-		    .lookup("java:comp/env/jdbc/TestDB");
-	    conexion = ds.getConnection();
-
-	    String sqlInsert = "INSERT INTO persona (nombre,apellidos,edad) VALUES ( ? , ? , ?)";
+	    String sqlInsert = "INSERT INTO persona (nombre,apellidos,edad, url_foto) VALUES ( ? , ? , ?, ?)";
 
 	    st = conexion.prepareStatement(sqlInsert);
 	    st.setString(1, pNombre);
 	    st.setString(2, pApellidos);
 	    st.setInt(3, 18);
-
+	    st.setString(4, pFoto);
 	    st.executeUpdate();
 	    correcto = true;
 
@@ -319,13 +359,7 @@ public class PersonaServlet extends HttpServlet {
 		}
 	    }
 	    // cerrar conexion
-	    if (conexion != null) {
-		try {
-		    conexion.close();
-		} catch (Exception e2) {
-		    e2.printStackTrace();
-		}
-	    }
+	    cerrarConexion(conexion);
 	}
 
 	return correcto;
