9c4b209d921fdb111af769c0430b1e10ed15f970
mario.alsol@gmail.com <mario.alsol@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-08 09:14:21 +0000

    Busqueda de personas en Linkedin mediante parse y guardarlos en una BD

---
+++ branches/marioLinkedinSearch/src/com/ipartek/formacion/linkedin/bean/LinkedInParse.java
@@ -0,0 +1,76 @@
+package com.ipartek.formacion.linkedin.bean;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class LinkedInParse {
+
+    private static String SEARCH_URL = "https://es.linkedin.com/pub/dir/?";
+    private String first;
+    private String last;
+
+    public LinkedInParse(String first, String last) {
+	super();
+	setFirst(first);
+	setLast(last);
+    }
+
+    public String getFirst() {
+	return first;
+    }
+
+    public String getLast() {
+	return last;
+    }
+
+    public void setFirst(String first) {
+	this.first = "first=" + first + "&";
+    }
+
+    public void setLast(String last) {
+	this.last = "last=" + last + "&search=Buscar";
+    }
+
+    public String getHtml() {
+	String resul = "";
+	String form = "";
+	try {
+	    Document doc = Jsoup.connect(SEARCH_URL + getFirst() + getLast())
+		    .get();
+	    Element listaResultados = doc.getElementById("result-set");
+	    if (listaResultados != null) {
+		String nombre = "";
+		String apellidos = "";
+		int cant = listaResultados.getElementsByClass("vcard").size();
+		for (int i = 0; i < cant; i++) {
+		    nombre = listaResultados.getElementsByClass("given-name")
+			    .get(i).text();
+		    apellidos = listaResultados
+			    .getElementsByClass("family-name").get(i).text();
+		    form = "<form action='persona' method='post'>";
+		    form += "<input type='hidden' name='nombre' value='"
+			    + nombre + "'>";
+		    form += "<input type='hidden' name='apellidos' value='"
+			    + apellidos + "'>";
+		    form += "<input type='hidden' name='operacion' value='1'>";
+		    form += "<input type='submit' value='Añadir'></form>";
+		    listaResultados.getElementsByClass("vcard").get(i)
+			    .append(form);
+		}
+
+		resul = listaResultados.html();
+
+	    } else {
+		resul = "<h1> 0 resultados</h1>";
+
+	    }
+
+	} catch (Exception e) {
+	    resul = e.getMessage();
+	    e.printStackTrace();
+	}
+
+	return resul;
+    }
+}
