b75d2e9f5c669d5ef34bdd2135e7159f2a1731ef
srnsecreta <srnsecreta@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-08 12:23:11 +0000

    Corregidos errores

--- branches/SergioLinkenInSearch/WebContent/resultados.jsp
+++ branches/SergioLinkenInSearch/WebContent/resultados.jsp
@@ -7,42 +7,14 @@
 
 <a href="index.jsp">Nueva búsqueda</a>
 
-<%
 
-	out.print("<hr>");
+	<jsp:include page="ficha.jsp" >
+		<jsp:param name="showid" value="1"/>
+		<jsp:param name="showButtonDelete" value="1"/>
+		<jsp:param name="showButtonSave" value="1"/>
+	</jsp:include>
 
 
-	if(request.getAttribute(Constantes.ATTR_LISTADO) != null) {
-		
-		List<Usuario> list = (List<Usuario>) request.getAttribute(Constantes.ATTR_LISTADO);
-		int index = 0;
-		
-		for(Usuario lst : list) {
-			out.print("Este es el index: " + index + "<br>");
-			out.print("Nombre: " + lst.getUsername() + "<br>") ;
-			out.print("Apellidos: " + lst.getApellidos());
-			out.print("<br>");
-			out.print("<img src='" + lst.getPhoto() + "' class='photo' height='60' width='60' alt='" + lst.getUsername() + " " + lst.getApellidos() + "'>");
-			out.print("<br>");
-			
-			%>
-			
-			<form action="user" method="post">	
-				<input type='hidden' value='A' name = 'action' />				
-		    	<input type='hidden' value='<%=lst.getUsername()%>' name = "nombre" />
-		    	<input type='hidden' value='<%=lst.getApellidos()%>' name = "apellidos" />
-		    	<input type='hidden' value='<%=lst.getPhoto()%>' name = "photo" />
-		    	<input type='submit' name="submit" value='Guardar' class='btn btn-outline btn-success' />
-			</form>
-			<%
-			
-			out.print("<br>");
-			index ++;	
-			
-			out.print("<hr>");
-		}
-	}
-%>
 
 <a href="index.jsp">Nueva búsqueda</a>
 
---
+++ branches/SergioLinkenInSearch/src/com/ipartek/formacion/busredsociales/comun/FactoriaMysql.java
@@ -0,0 +1,65 @@
+package com.ipartek.formacion.busredsociales.comun;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public final class FactoriaMysql {
+
+	private static final String STR_LOOKUP = "java:comp/env/jdbc/MyConexion";
+	
+	
+	private static Connection conexion = null;
+
+	/**
+	 * Constructor privado
+	 */
+	private FactoriaMysql() {
+
+	}
+
+	public static Connection conectar() throws Exception {
+
+		
+
+			if (conexion == null) {
+				/*
+				 * CONEXIÓN DIRECTA CON LA BASE DE DATOS
+				 * Class.forName("com.mysql.jdbc.Driver"); //Establecemos la
+				 * conexión con la base de datos. conexion = (Connection)
+				 * DriverManager.getConnection
+				 * ("jdbc:mysql://localhost/srncodesnippet","root", "");
+				 */
+
+				// CONEXIÓN USANDO DATASOURCE (xml en META-INF, e información en
+				// web.xml)
+				// Para obtener la conexión usando un pool de conexiones con
+				// datasource de Tomcat
+				InitialContext ctx = new InitialContext();
+				// en el web.xml se configura el jdbc/MyConexion
+				DataSource ds = (DataSource) ctx
+						.lookup("java:comp/env/jdbc/MyConexion");
+				conexion = ds.getConnection();
+
+				if (conexion == null) {
+					throw new Exception("No se ha creado la conexión");
+				}
+
+			}
+
+			return conexion;
+
+	}
+
+	public static void desconectar() throws SQLException {
+		if (conexion != null) {
+			if (!conexion.isClosed()) {
+				conexion.close();
+			}
+			conexion = null;
+		}
+	}
+
+}
