62e74fa674790ffa40dc0a89c0022abd9edcf754
mario.alsol@gmail.com <mario.alsol@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-12 09:55:40 +0000

    Borrar, actualizar e insetar funcionando. Había que poner a null después de cerrar la conexión.

--- branches/marioLinkedinSearch/src/com/ipartek/formacion/linkedin/modelo/dao/MySqlDAOFactory.java
+++ branches/marioLinkedinSearch/src/com/ipartek/formacion/linkedin/modelo/dao/MySqlDAOFactory.java
@@ -6,12 +6,33 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class MySqlDAOFactory extends DAOFactory {
+    // patron singleton para esta clase, @see:
+    // http://es.wikipedia.org/wiki/Singleton#Java
 
+    private static MySqlDAOFactory INSTANCE = null;
     private static Connection con = null;
     private static final String DATA_SOURCE = "java:comp/env/jdbc/TestDB";
 
-    public static Connection conectar() {
-	// patron singleton, si ya esta creada para que volver hacerlo ?
+    // constructor privado
+    private MySqlDAOFactory() {
+    }
+
+    // creador sincronizado para protegerse de posibles problemas multi-hilo
+    // otra prueba para evitar instanciación múltiple
+    private synchronized static void createInstance() {
+	if (INSTANCE == null) {
+	    INSTANCE = new MySqlDAOFactory();
+	}
+    }
+
+    public static MySqlDAOFactory getInstance() {
+	if (INSTANCE == null)
+	    createInstance();
+	return INSTANCE;
+    }
+
+    public Connection conectar() {
+
 	if (con == null) {
 	    try {
 		InitialContext ctx = new InitialContext();
@@ -24,11 +45,12 @@ public class MySqlDAOFactory extends DAOFactory {
 	return con;
     }
 
-    public static void desconectar() {
+    public void desconectar() {
 
 	if (con != null) {
 	    try {
 		con.close();
+		con = null;
 	    } catch (Exception e) {
 		e.printStackTrace();
 	    }
