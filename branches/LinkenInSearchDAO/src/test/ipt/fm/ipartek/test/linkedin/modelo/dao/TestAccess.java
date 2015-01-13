499fbc8d6484b9e27776cd72cf368ea84391941c
ander.ipartek@gmail.com <ander.ipartek@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-13 08:14:22 +0000

    initial import

---
+++ branches/LinkenInSearchDAO/src/test/ipt/fm/ipartek/test/linkedin/modelo/dao/TestAccess.java
@@ -0,0 +1,36 @@
+package ipt.fm.ipartek.test.linkedin.modelo.dao;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class TestAccess {
+
+	private static final String DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
+	private static final String CON_URL = "jdbc:odbc:TestDB";
+	
+	@Test
+	public void test()  {
+		
+		try{
+			Class.forName( DRIVER );  
+			Connection conexion = DriverManager.getConnection(CON_URL);  
+			Statement st = conexion.createStatement(); 
+			ResultSet rs = st.executeQuery("select * from persona");
+			while ( rs.next() ) {
+				System.out.println(rs.getObject(1)); 
+				System.out.println(rs.getObject(2)); 
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			fail("Excepcion concetnado a la BBDD");
+		}	
+				
+	}
+
+}
