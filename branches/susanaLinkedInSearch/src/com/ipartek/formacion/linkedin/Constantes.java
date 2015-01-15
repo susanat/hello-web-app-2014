13ef8c1218f9a3d54d43dfd565cee7a929a4b12b
susanacostoya@gmail.com <susanacostoya@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-15 11:29:56 +0000

    Nuevos cambios proyecto funcionando

---
+++ branches/susanaLinkedInSearch/src/com/ipartek/formacion/linkedin/Constantes.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.linkedin;
+
+public class Constantes {
+
+    public static final String PERSONA_BUSCADA = "persona";
+    public static final String JSP_INDEX = "index.jsp";
+
+}
