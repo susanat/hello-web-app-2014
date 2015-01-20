f2c459348382e2f48d5e5d4c4fdb9fe31147aa6d
srnsecreta <srnsecreta@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-20 12:25:51 +0000

    Initial import.

---
+++ branches/sergio/Inscripciones/src/com/ipartek/formacion/inscripciones/consulta/Mes.java
@@ -0,0 +1,37 @@
+package com.ipartek.formacion.inscripciones.consulta;
+
+public class Mes {
+
+	private String count;
+	private String month;
+	
+	public Mes() {
+				
+	}
+
+	public String getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public Mes(String count, String month) {
+		super();
+		this.count = count;
+		this.month = month;
+	}
+	
+	
+	
+
+}
