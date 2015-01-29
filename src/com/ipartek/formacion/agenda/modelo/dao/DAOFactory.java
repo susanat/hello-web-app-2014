68a64b888200bd0fb5d065590945f93841a1b91c
maitanecasfer@gmail.com <maitanecasfer@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-01-29 12:25:10 +0000

    Creacion de la interfaz IPersona y creacion del DAOFactory y MySql

--- src/com/ipartek/formacion/agenda/modelo/dao/DAOFactory.java
+++ src/com/ipartek/formacion/agenda/modelo/dao/DAOFactory.java
@@ -1,5 +1,33 @@
 package com.ipartek.formacion.agenda.modelo.dao;
 
+
+/**
+ * Creacion de la factoria abstracta (este archivo es para determinar el tipo de
+ * base de datos con el que vamos a trabajar
+ * 
+ * @author Susana COstoya y Kepa Escudero
+ *
+ */
+
 public abstract class DAOFactory {
 
+	// DAOs
+	public abstract IPersonaDAO getPersonaDAO();
+
+	// Tipos de BBDD
+	public final static int ACCESS = 1;
+	public final static int MYSQL = 2;
+
+	public static DAOFactory getDAOFactory(int tipo) {
+		switch (tipo) {
+		case MYSQL:
+			return MySqlDAOFactory.getInstance();
+			/*
+			 * case ACCESS: return new FactoriaAccess();
+			 */
+		default:
+			return null;
+		}
+
+	}
 }
---
+++ src/com/ipartek/formacion/agenda/modelo/dao/MySqlDAOFactory.java
@@ -0,0 +1,5 @@
+package com.ipartek.formacion.agenda.modelo.dao;
+
+public class MySqlDAOFactory {
+
+}
