c2a41093bad558ed956a1fea1198d6883706e774
ja.perez973@gmail.com <ja.perez973@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-02 10:45:34 +0000

    Listado de Constantes a utilizar por la aplicacion

---
+++ branches/AgendaOnline/src/com/ipartek/formacion/agenda/Constantes.java
@@ -0,0 +1,24 @@
+package com.ipartek.formacion.agenda;
+
+/**
+ * Contiene Constantes a utilizar en la agenda
+ * <ul>
+ * <li>Acciones a Ejecutar</li>
+ * <li>Nombre de las columnas de la vista de la agenda</li>
+ * </ul>
+ * 
+ * @author Curso
+ *
+ */
+public class Constantes {
+	// Listado de posibles acciones a ejecutar
+	public static final String ACC_KEY = "acc";
+	public static final String ACC_BUSCAR_CONTACTO = "f";
+	public static final String ACC_BUSCAR_CLEAR = "c";
+	public static final String ACC_ANADIR_CONTACTO = "a";
+	public static final String ACC_ELIMINAR_CONTACTO = "d";
+
+	// Servlet
+	public static final String CONTROLLER_AGENDA = "";
+
+}
