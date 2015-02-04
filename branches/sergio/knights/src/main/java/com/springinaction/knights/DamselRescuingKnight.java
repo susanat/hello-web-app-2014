af1dca129ecc48a32007a07bc5409a25a43c6a1f
srnsecreta <srnsecreta@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-04 14:45:19 +0000

    Corregida clase

--- branches/sergio/knights/src/main/java/com/springinaction/knights/DamselRescuingKnight.java
+++ branches/sergio/knights/src/main/java/com/springinaction/knights/DamselRescuingKnight.java
@@ -1,13 +1,26 @@
 package com.springinaction.knights;
 
 public class DamselRescuingKnight implements Knight {
-  private RescueDamselQuest quest;
-  
-  public DamselRescuingKnight() {
-    quest = new RescueDamselQuest(); //<co id="co_coupledToQuest"/>
-  }
-  
-  public void embarkOnQuest() throws QuestException {
-    quest.embark();
-  }
+	private Quest quest;
+	private int contQuest;
+
+	public DamselRescuingKnight(Quest quest) {
+		this.quest = quest; // <co id="co_injectedQuest"/>
+		this.contQuest = 0;
+	}
+
+	public void embarkOnQuest() throws QuestException {
+		quest.embark();
+		this.contQuest++;
+	}
+
+	public void setQuest(Quest quest) {
+		this.quest = quest;
+
+	}
+
+	public int getContQuest() {
+
+		return this.contQuest;
+	}
 }
