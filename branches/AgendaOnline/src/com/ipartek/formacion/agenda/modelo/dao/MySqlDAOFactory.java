d3479a0588428f7bc6d322c482f9d79356eff888
maitanecasfer@gmail.com <maitanecasfer@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-05 08:40:12 +0000

    Interfaz de MySQL

--- branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/MySqlDAOFactory.java
+++ branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/MySqlDAOFactory.java
@@ -1,5 +1,68 @@
 package com.ipartek.formacion.agenda.modelo.dao;
 
-public class MySqlDAOFactory {
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MySqlDAOFactory extends DAOFactory {
+
+	private static MySqlDAOFactory INSTANCE = null;
+	private static Connection con = null;
+	private static final String DATA_SOURCE = "java:comp/env/jdbc/agenda";
+
+	// constructor privado
+	private MySqlDAOFactory() {
+	}
+
+	// creador sincronizado para protegerse de posibles problemas multi-hilo
+	// otra prueba para evitar instanciación múltiple
+	private synchronized static void createInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new MySqlDAOFactory();
+
+		}
+	}
+
+	public static MySqlDAOFactory getInstance() {
+		if (INSTANCE == null)
+			createInstance();
+		return INSTANCE;
+	}
+
+	public Connection conectar() throws ModelException {
+
+		if (con == null) {
+			try {
+				InitialContext ctx = new InitialContext();
+				DataSource ds = (DataSource) ctx.lookup(DATA_SOURCE);
+				con = ds.getConnection();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("LA CONEXIÓN A LA BD, HA FALLADO");
+				throw new ModelException("LA CONEXIÓN A LA BD, HA FALLADO");
+			}
+		}
+		return con;
+	}
+
+	public void desconectar() {
+
+		if (con != null) {
+			try {
+				con.close();
+				con = null;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	@Override
+	public IPersonaDAO getPersonaDAO() {
+		return new PersonaMySqlDAO();
+	}
 
 }
