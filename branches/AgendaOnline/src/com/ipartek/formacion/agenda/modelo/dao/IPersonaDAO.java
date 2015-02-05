6df54a7500401d96edbb2286866c6e7260be6726
aritz.justice@gmail.com <aritz.justice@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-05 08:54:34 +0000

    corregidos nombres de la tabla y las columnas

--- branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/IPersonaDAO.java
+++ branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/IPersonaDAO.java
@@ -4,7 +4,7 @@ package com.ipartek.formacion.agenda.modelo.dao;
  * Interfaz IPersonaDAO, este archivo define las operaciones de CRUD contra la
  * tabla Persona de la base de datos (MySQL). Si en el futuro se usase otro tipo
  * de base de datos habria que cambiar este archivo
- * 
+ *
  * @author Susana Costoya, Kepa Escudero
  *
  */
@@ -22,14 +22,14 @@ import com.ipartek.formacion.agenda.bean.Persona;
  */
 public interface IPersonaDAO {
 
-	static final String TABLA = "persona";
-	static final String COL_ID = "id";
+	static final String TABLA = "contacto";
+	static final String COL_ID = "idcontacto";
 	static final String COL_NOMBRE = "nombre";
 	static final String COL_APELLIDOS = "apellidos";
-	static final String COL_TEL_FIJO = "tel fijo";
-	static final String COL_TEL_MOVIL = "tel movil";
+	static final String COL_TEL_FIJO = "telffijo";
+	static final String COL_TEL_MOVIL = "telfmovil";
 	static final String COL_DOMICILIO = "domicilio";
-	static final String COL_LOCALIDAD = "localidad";
+	static final String COL_LOCALIDAD = "poblacion";
 	static final String COL_PROVINCIA = "provincia";
 	static final String COL_CP = "cp";
 	static final String COL_ANOTACIONES = "anotaciones";
