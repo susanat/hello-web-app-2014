e526fe9ce52b8ee892408be4214b416d95766649
maitanecasfer@gmail.com <maitanecasfer@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-05 12:14:43 +0000

    Actualizacion de Persona y PersonaMySqlDAO

--- branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/PersonaMySqlDAO.java
+++ branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/PersonaMySqlDAO.java
@@ -1,7 +1,9 @@
 package com.ipartek.formacion.agenda.modelo.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -79,27 +81,206 @@ public class PersonaMySqlDAO implements IPersonaDAO {
 	}
 
 	@Override
-	public Persona getById(Persona p) throws ModelException {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized Persona getById(Persona p) {
+		Persona pers = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			conexion = MySqlDAOFactory.getInstance().conectar();
+
+			st = conexion.prepareStatement(SQL_SELECT_BYID);
+			st.setInt(1, p.getIdcontacto());
+
+			rs = st.executeQuery();
+
+			pers = new Persona(rs.getInt("idcontacto"), rs.getString("nombre"),
+					rs.getString("apellidos"), rs.getInt("telFijo"),
+					rs.getInt("telMovil"), rs.getString("poblacion"),
+					rs.getString("direccion"), rs.getString("provincia"),
+					rs.getInt("cp"));
+		} catch (Exception e) {
+			// cerrar conexion
+
+			e.printStackTrace();
+
+		} finally { // cerrar todos los objetos creados para el acceso de BBDD
+			// cerrar ResultSet
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			// cerrar statements
+			if (st != null) {
+				try {
+					st.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			MySqlDAOFactory.getInstance().desconectar();
+
+		}
+
+		return pers;
 	}
 
 	@Override
-	public int insert(Persona p) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+	public synchronized int insert(Persona p) throws ModelException {
+		int idNuevo = -1;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			conexion = MySqlDAOFactory.getInstance().conectar();
+			st = conexion.prepareStatement(SQL_INSERT,
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, p.getNombre());
+			st.setString(2, p.getApellidos());
+			st.setInt(3, p.getTelFijo());
+			st.setInt(4, p.getTelMovil());
+			st.setString(5, p.getPoblacion());
+			st.setString(6, p.getDireccion());
+			st.setString(7, p.getProvincia());
+			st.setInt(8, p.getCp());
+
+			int affectedRows = st.executeUpdate();
+			if (affectedRows == 0) {
+				throw new SQLException(
+						"Creating user failed, no rows affected.");
+			}
+
+			try (ResultSet generatedKeys = st.getGeneratedKeys()) {
+				if (generatedKeys.next()) {
+					idNuevo = generatedKeys.getInt(1);
+				} else {
+					throw new SQLException(
+							"Creating user failed, no ID obtained.");
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ModelException(e.getMessage());
+
+		} finally { // cerrar todos los objetos creados para el acceso de BBDD
+			// cerrar ResultSet
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			// cerrar statements
+			if (st != null) {
+				try {
+					st.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			MySqlDAOFactory.getInstance().desconectar();
+
+		}
+
+		return idNuevo;
 	}
 
 	@Override
-	public boolean delete(Persona p) throws ModelException {
-		// TODO Auto-generated method stub
-		return false;
+	public synchronized boolean delete(Persona p) {
+		boolean correcto = false;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			conexion = MySqlDAOFactory.getInstance().conectar();
+
+			st = conexion.prepareStatement(SQL_DELETE);
+			st.setInt(1, p.getIdcontacto());
+
+			st.executeUpdate();
+			correcto = true;
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			// cerrar conexion
+
+		} finally { // cerrar todos los objetos creados para el acceso de BBDD
+			// cerrar ResultSet
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			// cerrar statements
+			if (st != null) {
+				try {
+					st.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			MySqlDAOFactory.getInstance().desconectar();
+
+		}
+
+		return correcto;
 	}
 
 	@Override
-	public boolean update(Persona p) throws ModelException {
-		// TODO Auto-generated method stub
-		return false;
+	public synchronized boolean update(Persona p) {
+		boolean correcto = false;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			conexion = MySqlDAOFactory.getInstance().conectar();
+
+			st = conexion.prepareStatement(SQL_UPDATE);
+			st.setString(1, p.getNombre());
+			st.setString(2, p.getApellidos());
+			st.setInt(3, p.getTelFijo());
+			st.setInt(4, p.getTelMovil());
+			st.setString(5, p.getPoblacion());
+			st.setString(6, p.getDireccion());
+			st.setString(7, p.getProvincia());
+			st.setInt(8, p.getCp());
+
+			st.executeUpdate();
+			correcto = true;
+
+		} catch (Exception e) {
+			// cerrar conexion
+
+			e.printStackTrace();
+
+		} finally { // cerrar todos los objetos creados para el acceso de BBDD
+			// cerrar ResultSet
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			// cerrar statements
+			if (st != null) {
+				try {
+					st.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+			MySqlDAOFactory.getInstance().desconectar();
+
+		}
+
+		return correcto;
 	}
 
 }
