a5062e6f0ed487b23f5542bff2f60f4b4e846443
maitanecasfer@gmail.com <maitanecasfer@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-05 08:40:12 +0000

    Interfaz de MySQL

---
+++ branches/AgendaOnline/src/com/ipartek/formacion/agenda/modelo/dao/PersonaMySqlDAO.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.agenda.modelo.dao;
+
+import java.util.ArrayList;
+
+import com.ipartek.formacion.agenda.bean.Persona;
+
+public class PersonaMySqlDAO implements IPersonaDAO {
+
+	@Override
+	public ArrayList<Persona> getAll() throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Persona getById(Persona p) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(Persona p) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean delete(Persona p) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Persona p) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
