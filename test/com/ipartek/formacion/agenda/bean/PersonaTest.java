e0d881c1bc23431afd039efb84533b51c2ab1071
ja.perez973@gmail.com <ja.perez973@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-06 09:58:21 +0000

    Libreria de MySQL Inicio de las pruebas sobre el objeto Persona

--- test/com/ipartek/formacion/agenda/bean/PersonaTest.java
+++ test/com/ipartek/formacion/agenda/bean/PersonaTest.java
@@ -1,5 +1,8 @@
 package com.ipartek.formacion.agenda.bean;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -31,5 +34,54 @@ public class PersonaTest {
 		int n = Integer.parseInt(s);
 		// p.setCp(Integer.parseInt(s));
 	}
+
+
+	@Test
+	/**
+	 * Pruebas con el Nombre
+	 * <ul>
+	 * 	<li>Comprobar que no sea NULL</li>
+	 * 	<li>Comprobar que no sea vacio</li>
+	 * 	<li>Comprobar que empiece por una letra</li>
+	 * 	<li>Comprobar que contenga letras y '.'</li>
+	 *  <li>Otras...</li>
+	 * </ul>
+	 */
+	public void testSetNombre() {
+		Persona p=new Persona();
+		String nombre=null;
+		assertFalse("No vale un nombre 'NULL'", p.setNombre(nombre));
+		nombre="";
+		assertFalse("No vale un nombre ''", p.setNombre(nombre));
+
+		char c;
+		for (int i = 35; i < 256; i++) {
+			c = (char) i;
+			nombre = String.valueOf(c);
+			System.out.println(nombre);
+			if (Character.isLetter(c)) {
+				assertTrue(nombre + ": Debe comenzar con una letra el nombre",
+						p.setNombre(nombre));
+			} else {
+				assertFalse(nombre + ": Debe comenzar con una letra el nombre",
+						p.setNombre(nombre));
+			}
+		}
+
+		for (int i = 35; i < 256; i++) {
+			c = (char) i;
+			nombre = "a" + String.valueOf(c);
+			System.out.println(nombre);
+			if (Character.isLetter(c) || c == '.') {
+				assertTrue(nombre + ": Puede contener letras y '.' el nombre",
+						p.setNombre(nombre));
+			} else {
+				assertFalse(nombre + ": Puede contener letras y '.' el nombre",
+						p.setNombre(nombre));
+			}
+		}
+
+	}
+
 
 }
