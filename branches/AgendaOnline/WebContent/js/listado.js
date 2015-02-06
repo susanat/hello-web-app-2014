f9b7de3e465452a07e1e7193dacb09770f6e5c43
ja.perez973@gmail.com <ja.perez973@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-06 12:27:58 +0000

    Funcionalidad del Checked

--- branches/AgendaOnline/WebContent/js/listado.js
+++ branches/AgendaOnline/WebContent/js/listado.js
@@ -1,8 +1,46 @@
 $(document).ready(function() {
 	$('#idAgenda').DataTable({
 		"bFilter" : false,
-		"aoColumnDefs" : [
-			{ "bSortable": false, "aTargets": [ 0 ] },
-		]
+		"aoColumnDefs" : [ {
+			"bSortable" : false,
+			"aTargets" : [ 0 ]
+		}, ]
+	});
+
+	//Borramos la clase del la primera columna
+	$("#idAgenda tr").find('th:eq(0)').removeClass("sorting_asc");
+	//$("#idAgenda tr").find('th:eq(1)').toggleClass("sorting_asc");
+	//$("#idAgenda tr").find('td:eq(0)').removeClass("sorting_1");
+	//$("#idAgenda tr").find('td:eq(1)').toggleClass("sorting_1");
+	
+	//Funcion que pone/quita el valor de los checbox seleccionados/deseleccionados en el campo 'idEliminar' 
+	$('input[type="checkbox"]').change(function() {
+		//odtengo el valor de campo CheckBox
+		var value = $(this).val();
+		//odtengo el valor del campo IdEliminar
+		var sIdEliminar = $('#idEliminar').val();
+		var vIdEliminar = jQuery.makeArray(sIdEliminar.split(';'));
+
+		//Si el campo esta chequeado
+		if ($(this).prop('checked')) {
+			if (!sIdEliminar){
+				vIdEliminar[0]=value;
+			}else{
+				//Anado el valor al array de IDs a eliminar
+				vIdEliminar.push(value);				
+			}
+		}else{		
+			indEliminar=jQuery.inArray(value,vIdEliminar);
+			//Elimino el valor del array de IDs a eliminar
+			vIdEliminar.splice(indEliminar,1);
+			/*
+			vIdEliminar = jQuery.grep(vIdEliminar, function( a ) {
+				return a !== value;
+				});
+				*/			
+		}
+		//cargo los datos en el campo con los identificadores a eliminar
+		sIdEliminar=vIdEliminar.join(';');
+		$('#idEliminar').val(sIdEliminar);	
 	});
 });
