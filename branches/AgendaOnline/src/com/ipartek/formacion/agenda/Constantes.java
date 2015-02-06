ea918ef24107836cbf8ce8e0621f778de6dfe9f1
aritz.justice@gmail.com <aritz.justice@gmail.com@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-06 11:14:53 +0000

    añadida constante para actualizar

--- branches/AgendaOnline/src/com/ipartek/formacion/agenda/Constantes.java
+++ branches/AgendaOnline/src/com/ipartek/formacion/agenda/Constantes.java
@@ -6,7 +6,7 @@ package com.ipartek.formacion.agenda;
  * <li>Acciones a Ejecutar</li>
  * <li>Nombre de las columnas de la vista de la agenda</li>
  * </ul>
- * 
+ *
  * @author Curso
  *
  */
@@ -15,6 +15,7 @@ public class Constantes {
 	public static final String OP_KEY = "acc";
 	public static final String OP_BUSCAR = "f";
 	public static final String OP_ELIMINAR = "d";
+	public static final String OP_ACTUALIZAR = "u";
 
 	// Servlet
 	public static final String CONTROLLER_AGENDA = "";
