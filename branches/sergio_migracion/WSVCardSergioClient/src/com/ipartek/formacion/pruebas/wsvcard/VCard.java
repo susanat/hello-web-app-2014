adfff58337c8189f201bed93d69ef8e6a060efb9
srnsecreta <srnsecreta@94b3c656-dec0-bd6a-56b0-ea2964645cc4>
2015-02-16 11:47:15 +0000

    Initial import.

---
+++ branches/sergio_migracion/WSVCardSergioClient/src/com/ipartek/formacion/pruebas/wsvcard/VCard.java
@@ -0,0 +1,12 @@
+/**
+ * VCard.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.ipartek.formacion.pruebas.wsvcard;
+
+public interface VCard extends java.rmi.Remote {
+    public com.ipartek.formacion.pruebas.wsvcard.Persona getVCard(java.lang.String user, java.lang.String password) throws java.rmi.RemoteException;
+}
